8dffe7a63f7c5f617902dbe06833357b43d79b71
yupeng <91924990+yupengyao@users.noreply.github.com>
2023-06-14 15:56:09 -0500

    Update Example.py

--- Example.py
+++ Example.py
@@ -11,16 +11,17 @@ import matplotlib.pyplot as plt
 import sys, os
 from astropy.stats import biweight_scale
 
+# Input and preprecess the spectrum.
+indir = '/content/drive/MyDrive/Payne4MagE/payne_inputs/herc_12_multi.fits' 
+wavelength, spectrum, spectrum_err, wavelength_blaze, spectrum_blaze = preprocess_spectra(indir, 3500, 7000)
 
-path = 'drive/MyDrive/Payne4MagE/payne_inputs/herc_12_multi.fits' 
+# Spectra analyzing.
+rv0 = 80 # Radius velocity of the target.
+kernel_size = 40 # kernel size: 40 for MagE spectra and 10 for MIKE spectra.
+specfname = 'spec_' + indir[34:-5]
 outdir = '/content/drive/MyDrive/Payne4MagE/github_output'
-NNpath = '/content/drive/MyDrive/Payne4MagE/NN_normalized_spectra_float16_fixwave.npz'
+NNpath = '/content/drive/MyDrive/Payne4MagE/NN_normalized_spectra_float16_fixwave.npz' # Path of the emulator model.
+spectra_analyzing(specfname, outdir, NNpath, rv0, kernel_size, wavelength, spectrum, spectrum_err, wavelength_blaze, spectrum_blaze) # Analyzing the spectra.
 
-wavelength, spectrum, spectrum_err, wavelength_blaze, spectrum_blaze = preprocess_spectra(path, 3500, 7000)
-
-rv0 = 80
-kernel_size = 40
-specfname = 'spec_'+path[34:-5]
-spectra_analyzing(specfname, outdir, NNpath, rv0, kernel_size, wavelength, spectrum, spectrum_err, wavelength_blaze, spectrum_blaze)
-
+# Go through the result folder and make a summary table.
 summary(outdir)
